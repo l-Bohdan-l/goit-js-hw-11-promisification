bf2dcef0ebd213782637c15239f6da66e4234bf2
l-Bohdan-l <81586955+l-Bohdan-l@users.noreply.github.com>
2021-10-12 20:56:14 +0000

    Deploying to gh-pages from @ l-Bohdan-l/goit-js-hw-11-promisification@8c9f226a2aa975b7edeb5321be34320a3418914c 🚀

--- task-1.42efa467.js
+++ task-1.42efa467.js
@@ -1,6 +1,4 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"LLK9":[function(require,module,exports) {
-const e=e=>{return new Promise((t,n)=>{setTimeout(()=>{t(e),n()},e)})},t=e=>console.log(`Resolved after ${e}ms`);e(2e3).then(t),e(1e3).then(t),e(1500).then(t);
-},{}],"Focm":[function(require,module,exports) {
-"use strict";require("./js/task-1.js");
-},{"./js/task-1.js":"LLK9"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-11-promisification/src.b0316a68.js.map
+const e=e=>{return new Promise(t=>{setTimeout(()=>{t(e)},e)})},t=e=>console.log(`Resolved after ${e}ms`);e(2e3).then(t),e(1e3).then(t),e(1500).then(t);
+},{}]},{},["LLK9"], null)
+//# sourceMappingURL=/goit-js-hw-11-promisification/task-1.42efa467.js.map
